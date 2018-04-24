5b9698b86eb0ed818b96d14fbc1623a61debc95d
Aaron G <aaronhgross@gmail.com>
2018-04-24 10:21:33 -0400

    fixed contact icons

--- js/index3.js
+++ js/index3.js
@@ -3,7 +3,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
+            
         });
     });
 });
@@ -13,28 +14,34 @@ let state = 'on';
 let initMap = () => {
 
     let map = new google.maps.Map(document.getElementById('googleMap'), {
-      zoom: 13,
-      disableDefaultUI: true,
-      draggable: false,
-      zoomControl: false, 
-      scrollwheel: false, 
-      disableDoubleClickZoom: true,
-      center: {lat: 33.746, lng: -84.37}
+        zoom: 13,
+        disableDefaultUI: true,
+        draggable: false,
+        zoomControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        center: {
+            lat: 33.746,
+            lng: -84.37
+        }
     });
 
     let marker = new google.maps.Marker({
-      position: {lat: 33.75, lng: -84.389},
-      map: map,
-      title: 'Hello World!'
+        position: {
+            lat: 33.75,
+            lng: -84.389
+        },
+        map: map,
+        title: 'Hello World!'
     });
 
-    
+
     return marker;
 }
 
 let marker = initMap()
 let toggleMarkerAnimation = (marker) => {
-    if(state === 'on') {
+    if (state === 'on') {
         marker.setAnimation(null)
         state = 'off';
     } else if (state = 'false') {
@@ -45,4 +52,6 @@ let toggleMarkerAnimation = (marker) => {
 
 toggleMarkerAnimation(marker);
 
-setInterval(() => { toggleMarkerAnimation(marker)}, 2500)
+setInterval(() => {
+    toggleMarkerAnimation(marker)
+}, 2500)
