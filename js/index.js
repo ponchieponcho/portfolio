442309e0852c6e3eb84b0b9739b9cf0d81c6482b
Aaron G <aaronhgross@gmail.com>
2018-04-25 19:22:34 -0400

    cleaning up files

--- js/index.js
+++ js/index.js
@@ -3,7 +3,55 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
 
         document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            behavior: 'smooth',
+            
         });
     });
 });
+
+let state = 'on';
+
+let initMap = () => {
+
+    let map = new google.maps.Map(document.getElementById('googleMap'), {
+        zoom: 13,
+        disableDefaultUI: true,
+        draggable: false,
+        zoomControl: false,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        center: {
+            lat: 33.75,
+            lng: -84.39
+        }
+    });
+
+    let marker = new google.maps.Marker({
+        position: {
+            lat: 33.75,
+            lng: -84.389
+        },
+        map: map,
+        title: 'Hello World!'
+    });
+
+
+    return marker;
+}
+
+let marker = initMap()
+let toggleMarkerAnimation = (marker) => {
+    if (state === 'on') {
+        marker.setAnimation(null)
+        state = 'off';
+    } else if (state = 'false') {
+        marker.setAnimation(google.maps.Animation.BOUNCE)
+        state = 'on';
+    }
+}
+
+toggleMarkerAnimation(marker);
+
+setInterval(() => {
+    toggleMarkerAnimation(marker)
+}, 2500)
